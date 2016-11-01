3d8294adb97fd760e8f5d5dd5e8102f9edc81b4d
Matthew Harris <matthew@biofare.org>
2016-11-01 16:36:04 -0700

    Vertical Mapping Fixed
    But horizontal is broken now - gotta fix

--- Final Project (Retry)/Battleship/battleship.cpp
+++ Final Project (Retry)/Battleship/battleship.cpp
@@ -22,6 +22,8 @@ battleship::~battleship()
 
 void battleship::on_playerGrid_cellClicked(int row, int column)
 {
+
+    // clear the grid every click
     for(int i = 0; i < 10; i++) {
         for(int j = 0; j < 10; j++) {
             ui->playerGrid->setItem(i,j, new QTableWidgetItem);
@@ -29,32 +31,56 @@ void battleship::on_playerGrid_cellClicked(int row, int column)
         }
     }
 
-
-    if(column == 0 || column == 9) {
-
+    // if selection is column A or J and horizontal
+    if((column == 0 || column == 9) && axisSelection == 0) {
+        // if selection is column A
         if(column == 0) {
             ui->playerGrid->setItem(row, column, new QTableWidgetItem);
             ui->playerGrid->item(row,column)->setBackground(QColor::fromRgb(255,80,91));
 
             ui->playerGrid->setItem(row, column + 1, new QTableWidgetItem);
             ui->playerGrid->item(row,column + 1)->setBackground(QColor::fromRgb(255,80,91));
-            ui->label->setText("Not acceptable");
+            ui->label->setText("NA");
         }
 
+        // if selection is column J
         else {
             ui->playerGrid->setItem(row, column - 1, new QTableWidgetItem);
             ui->playerGrid->item(row,column - 1)->setBackground(QColor::fromRgb(255,80,91));
-            ui->label->setText("Not acceptable");
+            ui->label->setText("NA");
+        }
+    }
+
+
+    // if selection is row 1 or 9 and vertical
+    else if ((row == 0 || row == 9) && axisSelection == 1) {
+
+        // if selection is row 1
+        if(row == 0) {
+            ui->playerGrid->setItem(row, column, new QTableWidgetItem);
+            ui->playerGrid->item(row,column)->setBackground(QColor::fromRgb(255,80,91));
+
+            ui->playerGrid->setItem(row + 1, column, new QTableWidgetItem);
+            ui->playerGrid->item(row + 1,column)->setBackground(QColor::fromRgb(255,80,91));
+            ui->label->setText("NA");
+        }
+
+        else {
+            ui->playerGrid->setItem(row - 1, column, new QTableWidgetItem);
+            ui->playerGrid->item(row - 1,column)->setBackground(QColor::fromRgb(255,80,91));
+            ui->label->setText("NA");
         }
     }
 
     else {
         if(axisSelection == 0) {
-            ui->playerGrid->setItem(row, column + 1, new QTableWidgetItem);
-            ui->playerGrid->item(row,column + 1)->setBackground(QColor::fromRgb(176,215,255));
+            if(column == 0) {
+                ui->playerGrid->setItem(row, column + 1, new QTableWidgetItem);
+                ui->playerGrid->item(row,column + 1)->setBackground(QColor::fromRgb(176,215,255));
 
-            ui->playerGrid->setItem(row,column - 1, new QTableWidgetItem);
-            ui->playerGrid->item(row,column - 1)->setBackground(QColor::fromRgb(176,215,255));
+                ui->playerGrid->setItem(row,column - 1, new QTableWidgetItem);
+                ui->playerGrid->item(row,column - 1)->setBackground(QColor::fromRgb(176,215,255));
+            }
         }
 
         else {
@@ -82,8 +108,6 @@ void battleship::on_verticalSwitch_clicked()
 {
     axisSelection = 1;
 }
-
-
 
 void battleship::on_horizontalSwitch_clicked()
 {
